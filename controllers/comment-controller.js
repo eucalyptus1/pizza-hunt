434b29426c147afd2d0250732bd5695af65a7958
euc1 <jamesharrington585@hotmail.com>
2022-04-02 19:27:15 -0400

    yeah

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,8 +1,8 @@
 const { Comment, Pizza } = require('../models');
 
 const commentController = {
-   // add comment to pizza
-   addComment({ params, body }, res) {
+  // add comment to pizza
+  addComment({ params, body }, res) {
     console.log(body);
     Comment.create(body)
       .then(({ _id }) => {
@@ -12,6 +12,19 @@ const commentController = {
           { new: true }
         );
       })
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.json(err));
+  },
+
+  // add reply to comment
+  addReply({ params, body }, res) {
+    Comment.findOneAndUpdate({ _id: params.commentId }, { $push: { replies: body } }, { new: true })
       .then(dbPizzaData => {
         if (!dbPizzaData) {
           res.status(404).json({ message: 'No pizza found with this id!' });
@@ -42,6 +55,16 @@ const commentController = {
         }
         res.json(dbPizzaData);
       })
+      .catch(err => res.json(err));
+  },
+  // remove reply
+  removeReply({ params }, res) {
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      { $pull: { replies: { replyId: params.replyId } } },
+      { new: true }
+    )
+      .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => res.json(err));
   }
 };
